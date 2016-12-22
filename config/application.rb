abf6edc52f14049e86a848dd134354fe996a7948
rombod madgidi <rambodmagid@yahoo.com>
2016-12-22 16:28:24 -0700

    commit 23456

--- config/application.rb
+++ config/application.rb
@@ -23,4 +23,5 @@ module Pecweb
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
+  config.serve_static_assets = true
 end
